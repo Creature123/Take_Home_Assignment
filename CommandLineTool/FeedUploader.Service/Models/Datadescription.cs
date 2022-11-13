3c9393247288c4411ea314dfff81d81e4f6cc13b
Sourav Das <sourav.besu.it.001@gmail.com>
2022-11-13 12:08:36 +0530

    Initial Project Setup Completed!
    - Added "SystemCommandLine" utility Package.
    - Created rootCommands, Import Command,helper Command
    
    -- run the project as below :
    
    open Terminal from FeedUploaderCLI project
    
    - dotnet build
    
    to run the Project
    
    - dotnet run -- --help for ( Help Command )
    
    Description:
      Command Line tool to import Project file from different sources :
    
    Usage:
      FeedUploaderCLI [command] [options]
    
    Options:
      --version       Show version information
      -?, -h, --help  Show help and usage information
    
    Commands:
      i, import  Use this command to import files

---
+++ CommandLineTool/FeedUploader.Service/Models/Datadescription.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FeedUploader.Service.Models
+{
+	public class Datadescription
+	{
+        public string tags { get; set; }
+        public string name { get; set; }
+        public string twitter { get; set; }
+    }
+}
+
---
+++ CommandLineTool/FeedUploaderCLI/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.CommandLine;
+using System.Threading.Tasks;
+
+namespace FeedUploaderCLI
+{
+    class Program
+    {
+        static async  Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand(description: "Command Line tool to import Project file from different sources :");
+
+            var importCommand = new Command("import", "Use this command to import files");
+            importCommand.AddAlias("i");
+            rootCommand.Add(importCommand);
+
+            var fileOption = new Option<string>
+               ("--filepath", "An option to pass filepath");
+            fileOption.IsRequired = true;
+            fileOption.AddAlias("--f");
+            fileOption.AddAlias("--file");
+
+            var capterraCommand = new Command("capterra", "Use this command to import files from capterra site");
+            capterraCommand.AddAlias("-c");
+            capterraCommand.AddOption(fileOption);
+
+
+            var softwareadviceCommand = new Command("softwareadvice", "Use this command to import files from softwaredevice site");
+            softwareadviceCommand.AddAlias("-s");
+            softwareadviceCommand.AddOption(fileOption);
+
+
+
+
+
+            importCommand.Add(
+                capterraCommand
+                );
+
+            importCommand.Add(
+                softwareadviceCommand
+                );
+
+
+
+
+            capterraCommand.SetHandler((optionArgumentvalue) =>
+            {
+                Console.WriteLine($"Capterra File option {optionArgumentvalue}");
+            }, fileOption);
+
+
+            softwareadviceCommand.SetHandler((optionArgumentvalue) =>
+            {
+                Console.WriteLine($"softwareadvice File option {optionArgumentvalue}");
+            }, fileOption);
+
+
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
+
